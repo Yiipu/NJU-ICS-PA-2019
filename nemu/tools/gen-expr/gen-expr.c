908e22cc3fa0c488d67adacec1e75ac48d51e380
202115517-Jin Yapu <jaap20035@outlook.com>
2024-09-20 15:33:23 +0800

    p: 表达式生成器

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,24 +1,78 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
-static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+#define BUFFER_SIZE 128
+static char buf[BUFFER_SIZE];
+static int pos = 0;
+void gen_rand_expr();
+
+void gen_num() {
+  int num = rand() % 100; // 0~99
+#ifdef ALLOW_NEG
+  if (rand() % 2 == 0) {
+    buf[pos++] = '-';
+  }
+#endif
+  pos += sprintf(buf + pos, "%d", num);
 }
 
+void gen_rand_op() {
+  switch (rand() % 4) {
+  case 0:
+    buf[pos++] = '+';
+    break;
+  case 1:
+    buf[pos++] = '-';
+    break;
+  case 2:
+    buf[pos++] = '*';
+    break;
+  case 3:
+    buf[pos++] = '/';
+    break;
+  }
+}
+
+void gen_rand_expr() {
+  if (pos >= BUFFER_SIZE - 5) {
+    gen_num();
+    return;
+  }
+  int choice = rand() % 3;
+  switch (choice) {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    buf[pos++] = '(';
+    gen_rand_expr();
+    buf[pos++] = ')';
+    break;
+  case 2:
+    gen_rand_expr();
+    gen_rand_op();
+    gen_rand_expr();
+    break;
+  }
+  buf[pos] = '\0';
+}
 static char code_buf[65536];
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+static char *code_format = "#include <stdio.h>\n"
+                           "int main() { "
+#ifdef ALLOW_NEG
+                           "  int result = %s; "
+                           "  printf(\"%%d\", result); "
+#else
+                           "  unsigned result = %s; "
+                           "  printf(\"%%u\", result); "
+#endif
+                           "  return 0; "
+                           "}";
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
@@ -28,7 +82,8 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
+  for (i = 0; i < loop; i++) {
+    pos = 0;
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
@@ -39,16 +94,21 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
     int result;
-    fscanf(fp, "%d", &result);
+    if (fscanf(fp, "%d", &result)) {
+#ifdef ALLOW_NEG
+      printf("%d %s\n", result, buf);
+#else
+      printf("%u %s\n", result, buf);
+#endif
+    }
     pclose(fp);
-
-    printf("%u %s\n", result, buf);
   }
   return 0;
 }
