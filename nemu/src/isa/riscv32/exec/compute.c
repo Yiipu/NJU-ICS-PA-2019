8eff780514d5bcab15770b6731ea308ab99747ac
tracer-ics2019 <tracer@njuics.org>
2024-09-21 22:58:50 +0800

    >  compile 202115517 opal Linux Y7000P-OK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:58:50 up  7:34,  0 users,  load average: 0.14, 0.11, 0.09 e67227efdd128227b1c9fb7fa07b931da3d52e2f

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -8,14 +8,14 @@ make_EHelper(lui) {
 }
 
 make_EHelper(addi) {
-  rtl_addi(&id_dest->reg, &id_src2->imm, id_src->val);
-  rtl_sr(id_dest->reg, &id_dest->reg, 4);
+  rtl_addi(&t0, &id_src2->imm, id_src->val);
+  rtl_sr(id_dest->reg, &t0, 4);
 
   print_asm_template3(addi);
 }
 
 make_EHelper(auipc) {
-  rtl_addi(&id_dest->reg, &cpu.pc, id_src->imm);
-  rtl_sr(id_dest->reg, &id_dest->reg, 4);
+  rtl_addi(&t0, &cpu.pc, id_src->imm);
+  rtl_sr(id_dest->reg, &t0, 4);
   print_asm_template2(auipc);
 }
