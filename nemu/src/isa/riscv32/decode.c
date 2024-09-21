c99af475eace04f6f69c6dd6469c42da254ecd2e
202115517-Jin Yapu <jaap20035@outlook.com>
2024-09-22 02:05:36 +0800

    优化汇编输出

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -27,8 +27,6 @@ make_DHelper(I) {
   decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
-
-  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", decinfo.isa.instr.simm11_0);
 }
 
 make_DHelper(S) {
--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -135,9 +135,5 @@ void isa_exec(vaddr_t *pc) {
   decinfo.isa.instr.val = instr_fetch(pc, 4);
   // 不支持压缩指令，所有 opcode 低两位必须为 0b11
   assert(decinfo.isa.instr.opcode1_0 == 0x3);
-  // 打印 val opcode6_2 funct3
-  printf("val: 0x%08x, opcode6_2: 0x%02x, funct3: 0x%02x\n",
-         decinfo.isa.instr.val, decinfo.isa.instr.opcode6_2,
-         decinfo.isa.instr.funct3);
   idex(pc, &opcode_table[decinfo.isa.instr.opcode6_2]);
 }
