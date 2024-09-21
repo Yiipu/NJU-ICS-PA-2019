b392cfc423668490903cf20b2c0a915dc5c1e510
202115517-Jin Yapu <jaap20035@outlook.com>
2024-09-21 22:21:21 +0800

    auipi: check_reg_index 断言失败，传入寄存器序号是 pc 值

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -9,12 +9,12 @@ make_EHelper(lui) {
 make_EHelper(addi) {
   rtl_addi(&id_dest->reg, &id_src2->imm, id_src->val);
   rtl_sr(id_dest->reg, &id_dest->reg, 4);
-  
+
   print_asm_template3(addi);
 }
 
-make_EHelper(auipc){
-  rtl_addi(&id_dest->reg, &decinfo.seq_pc, id_src->val);
+make_EHelper(auipc) {
+  rtl_addi(&id_dest->reg, 0, decinfo.seq_pc);
   rtl_sr(id_dest->reg, &id_dest->reg, 4);
 
   print_asm_template2(auipc);
