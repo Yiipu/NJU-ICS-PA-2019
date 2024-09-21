2d96f5bf4812ed54e93c3585d8b6ef38849fb522
tracer-ics2019 <tracer@njuics.org>
2024-09-21 19:03:37 +0800

    >  compile 202115517 opal Linux Y7000P-OK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:03:37 up  3:39,  0 users,  load average: 0.21, 0.10, 0.06 a023204db5de57972036e47b12965a6279a8b00f

--- nemu/src/isa/riscv32/exec/all-instr.h
+++ nemu/src/isa/riscv32/exec/all-instr.h
@@ -2,7 +2,7 @@
 
 make_EHelper(lui);
 
-make_EHelper(ld);
+make_EHelper(ld); // ?? LD 是 RISCV64 的指令，为什么在 RISCV32 中实现了呢？
 make_EHelper(st);
 
 make_EHelper(inv);
--- nemu/src/isa/riscv32/exec/exec.c
+++ nemu/src/isa/riscv32/exec/exec.c
@@ -1,6 +1,20 @@
 #include "cpu/exec.h"
 #include "all-instr.h"
 
+// clang-format off
+
+/*
+000'b LB: Load Byte
+001'b LH: Load Half
+010'b LW: Load Word
+011'b LD: RV64 Load Double
+100'b LBU: Load Byte Unsigned
+101'b LHU: Load Half Unsigned
+110'b LWU: RV64 Load Word Unsigned
+*/
+// static OpcodeEntry load_table [8] = {
+//   EXW(lb,1), EXW(lh,2), EXW(lw, 4), EMPTY, EXW(lbu, 1), EXW(lhu, 2), EMPTY, EMPTY
+// };
 static OpcodeEntry load_table [8] = {
   EMPTY, EMPTY, EXW(ld, 4), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
 };
@@ -27,9 +41,15 @@ static OpcodeEntry opcode_table [32] = {
   /* b10 */ EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
   /* b11 */ EMPTY, EMPTY, EX(nemu_trap), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
 };
+// clang-format on
 
 void isa_exec(vaddr_t *pc) {
   decinfo.isa.instr.val = instr_fetch(pc, 4);
+  // 不支持压缩指令，所有 opcode 低两位必须为 0b11
   assert(decinfo.isa.instr.opcode1_0 == 0x3);
+  // 打印指令
+  print_asm_template1(decinfo.isa.instr_name);
+  // 打印 val opcode6_2 funct3
+  printf("val: 0x%08x, opcode6_2: 0x%02x, funct3: 0x%02x\n", decinfo.isa.instr.val, decinfo.isa.instr.opcode6_2, decinfo.isa.instr.funct3);
   idex(pc, &opcode_table[decinfo.isa.instr.opcode6_2]);
 }
