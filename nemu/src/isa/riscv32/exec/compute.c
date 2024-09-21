b3f64a2c9854b93b338de8f7c14bdbc7772c4f5f
202115517-Jin Yapu <jaap20035@outlook.com>
2024-09-21 22:04:02 +0800

    debug: 寄存器值没有变化

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -8,12 +8,14 @@ make_EHelper(lui) {
 
 make_EHelper(addi) {
   rtl_addi(&id_dest->reg, &id_src2->imm, id_src->val);
+  rtl_sr(id_dest->reg, &id_dest->reg, 4);
   
   print_asm_template3(addi);
 }
 
 make_EHelper(auipc){
   rtl_addi(&id_dest->reg, &decinfo.seq_pc, id_src->val);
+  rtl_sr(id_dest->reg, &id_dest->reg, 4);
 
   print_asm_template2(auipc);
 }
