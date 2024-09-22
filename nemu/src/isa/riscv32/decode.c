fa5179e1a1eadede99bb15ed9ffcdc751743bf5e
202115517-Jin Yapu <jaap20035@outlook.com>
2024-09-22 09:12:20 +0800

    更正 B 型指令, 优化汇编输出

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -1,5 +1,6 @@
 #include "cpu/decode.h"
 #include "rtl/rtl.h"
+#include <stdint.h>
 
 // decode operand helper
 #define make_DopHelper(name)                                                   \
@@ -24,40 +25,34 @@ static inline make_DopHelper(r) {
 }
 
 make_DHelper(I) {
-  decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
+  int32_t simm = decinfo.isa.instr.simm11_0;
+  simm = (simm << 20) >> 20;
+
+  decode_op_i(id_src2, simm, true);
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
 
-make_DHelper(S) {
-  decode_op_r(id_src, decinfo.isa.instr.rs2, true);
-  int32_t simm = (decinfo.isa.instr.simm11_5 << 5) | decinfo.isa.instr.imm4_0;
-  decode_op_i(id_src2, simm, true);
-
-  print_Dop(id_src->str, OP_STR_SIZE, "%d(%s)", id_src2->val,
-            reg_name(id_src->reg, 4));
-
-  rtl_add(&id_src->addr, &id_src->val, &id_src2->val);
-
-  decode_op_r(id_dest, decinfo.isa.instr.rs1, true);
-}
-
 make_DHelper(U) {
-  decode_op_i(id_src, decinfo.isa.instr.imm31_12 << 12, true);
+  int32_t simm = decinfo.isa.instr.imm31_12 << 12;
+  decode_op_i(id_src, simm, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 
   print_Dop(id_src->str, OP_STR_SIZE, "0x%x", decinfo.isa.instr.imm31_12);
 }
 
-make_DHelper(J){
-  int32_t imm = (decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm11_ << 11) | (decinfo.isa.instr.imm10_1 << 1) | (decinfo.isa.instr.simm20 << 20);
-  decode_op_i(id_src, imm, true);
+make_DHelper(J) {
+  int32_t simm =
+      ((decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm11_ << 11) |
+       (decinfo.isa.instr.imm10_1 << 1) | (decinfo.isa.instr.simm20 << 20));
+  simm = (simm << 11) >> 11;
+  decode_op_i(id_src, simm, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 
-  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", imm);
+  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", simm);
 }
 
-make_DHelper(R){
+make_DHelper(R) {
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_r(id_src2, decinfo.isa.instr.rs2, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
@@ -66,12 +61,17 @@ make_DHelper(R){
   print_Dop(id_src2->str, OP_STR_SIZE, "%s", reg_name(id_src2->reg, 4));
 }
 
-make_DHelper(B){
-  int32_t imm = (decinfo.isa.instr.simm12 << 12) | (decinfo.isa.instr.imm10_5 << 5) | (decinfo.isa.instr.imm4_1 << 1) | (decinfo.isa.instr.simm12 << 13);
-  decode_op_i(id_src, imm, true);
-  decode_op_r(id_dest, decinfo.isa.instr.rs1, true);
+make_DHelper(B) {
+  int32_t imm =
+      (decinfo.isa.instr.simm12 << 12) | (decinfo.isa.instr.imm10_5 << 5) |
+      (decinfo.isa.instr.imm4_1 << 1) | (decinfo.isa.instr.imm11 << 11);
+  imm = (imm << 19) >> 19;
+  imm = imm + cpu.pc;
+  decode_op_i(id_dest, imm, true);
+  decode_op_r(id_src, decinfo.isa.instr.rs1, true);
+  decode_op_r(id_src2, decinfo.isa.instr.rs2, true);
 
-  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", imm);
+  print_Dop(id_dest->str, OP_STR_SIZE, "0x%x", imm);
 }
 
 make_DHelper(ld) {
--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -4,7 +4,9 @@ make_EHelper(jal) {
   rtl_li(&t0, cpu.pc + 4);
   rtl_sr(id_dest->reg, &t0, 4);
 
-  rtl_j(cpu.pc + id_src->val);
+  t0 = id_src->val + cpu.pc;
+  rtl_j(t0);
+  print_Dop(id_dest->str, OP_STR_SIZE, "0x%x", t0);
   print_asm_template2(jal);
 }
 
@@ -12,36 +14,38 @@ make_EHelper(jalr) {
   rtl_li(&t0, cpu.pc + 4);
   rtl_sr(id_dest->reg, &t0, 4);
 
-  rtl_j(id_src->val + id_src2->imm);
+  t0 = id_src->val + cpu.pc;
+  rtl_j(t0);
+  print_Dop(id_dest->str, OP_STR_SIZE, "0x%x", t0);
   print_asm_template2(jalr);
 }
 
 make_EHelper(beq) {
-  rtl_jrelop(RELOP_EQ, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_EQ, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(beq);
 }
 
 make_EHelper(bne) {
-  rtl_jrelop(RELOP_NE, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_NE, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(bne);
 }
 
 make_EHelper(blt) {
-  rtl_jrelop(RELOP_LT, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_LT, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(blt);
 }
 
 make_EHelper(bge) {
-  rtl_jrelop(RELOP_GE, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_GE, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(bge);
 }
 
 make_EHelper(bltu) {
-  rtl_jrelop(RELOP_LTU, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_LTU, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(bltu);
 }
 
 make_EHelper(bgeu) {
-  rtl_jrelop(RELOP_GEU, &id_src->val, &id_src2->val, id_dest->imm);
+  rtl_jrelop(RELOP_GEU, &id_src->val, &id_src2->val, id_dest->addr);
   print_asm_template3(bgeu);
 }
