36148002dcf8472b86746c765f4b0ccfc58a2c77
tracer-ics2019 <tracer@njuics.org>
2024-09-25 14:24:22 +0800

    >  run 202115517 opal Linux Y7000P-OK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:24:22 up 15 min,  0 users,  load average: 0.34, 0.17, 0.06 4409e1a57bad079b45d809bf3fedfa78ba8b6502

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,20 +3,46 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+    char buf[4096];
+    int len = vsprintf(buf, fmt, ap);
+    if (n == 0) {
+        return len;
+    }
+    if (len >= n) {
+        len = n - 1;
+    }
+    memcpy(out, buf, len);
+    out[len] = '\0';
+    return len;
+}
+
 int printf(const char *fmt, ...) {
-  return 0;
+    va_list args;
+    va_start(args, fmt);
+    int result = printk(fmt, args);
+    va_end(args);
+    return result;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  
+    return vsnprintf(out, 0, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+    va_list args;
+    va_start(args, fmt);
+    int result = vsnprintf(out, 0, fmt, args);
+    va_end(args);
+    return result;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+    va_list args;
+    va_start(args, fmt);
+    int result = vsnprintf(out, n, fmt, args);
+    va_end(args);
+    return result;
 }
 
 #endif
