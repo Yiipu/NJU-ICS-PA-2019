bc1fe0e53c1f40965cf790cdd067f951ff1c30a8
tracer-ics2019 <tracer@njuics.org>
2024-09-25 14:56:49 +0800

    >  run 202115517 opal Linux Y7000P-OK 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:56:49 up 47 min,  0 users,  load average: 0.05, 0.08, 0.08 4106915931acac036af481e7fcb8d36bba4c8708

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,46 +3,72 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-    char buf[4096];
-    int len = vsprintf(buf, fmt, ap);
-    if (n == 0) {
-        return len;
+static char NUM_CHAR[] = "0123456789ABCDEF";
+int vsnprintf(char * out, size_t n, const char * fmt, va_list ap) {
+  int len = 0;
+  char buf[128];
+  int buf_len = 0;
+  while (*fmt != '\0' && len < n) {
+    switch (*fmt) {
+    case '%':
+      fmt++;
+      switch (*fmt) {
+      case 'd': {
+        int val = va_arg(ap, int);
+        if (val == 0) out[len++] = '0';
+        if (val < 0) {
+          out[len++] = '-';
+          val = 0 - val;
+        }
+        for (buf_len = 0; val; val /= 10, buf_len++) {
+          buf[buf_len] = NUM_CHAR[val % 10]; // 这里buf会是逆序的
+        }
+        for (int i = buf_len - 1; i >= 0; i--) {
+          out[len++] = buf[i];
+        }
+        break;
+      } break; // case % 的break.
+      case '\n':
+        out[len++] = '\n';
+        break;
+      default:
+        out[len++] = *fmt;
+      }
+      fmt++;
     }
-    if (len >= n) {
-        len = n - 1;
-    }
-    memcpy(out, buf, len);
     out[len] = '\0';
     return len;
+  }
 }
 
-int printf(const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    int result = printk(fmt, args);
-    va_end(args);
-    return result;
+int printf(const char * fmt, ...) {
+  char buffer[1024];
+  va_list arg;
+  va_start(arg, fmt);
+  int result = vsprintf(buffer, fmt, arg);
+  printk(buffer);
+  va_end(arg);
+  return result;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-    return vsnprintf(out, (size_t)-1, fmt, ap);
+int vsprintf(char * out, const char * fmt, va_list ap) {
+  return vsnprintf(out, (size_t)-1, fmt, ap);
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    int result = vsnprintf(out, (size_t)-1, fmt, args);
-    va_end(args);
-    return result;
+int sprintf(char * out, const char * fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  int result = vsprintf(out, fmt, args);
+  va_end(args);
+  return result;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    int result = vsnprintf(out, n, fmt, args);
-    va_end(args);
-    return result;
+int snprintf(char * out, size_t n, const char * fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  int result = vsnprintf(out, n, fmt, args);
+  va_end(args);
+  return result;
 }
 
 #endif
