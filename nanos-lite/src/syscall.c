d56fda2389e069ff466fb89c7076faaa08544e76
202115517-Jin Yapu <jaap20035@outlook.com>
2025-01-17 00:28:52 +0800

    实现 SYS_yield

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,15 @@
-#include "common.h"
 #include "syscall.h"
+#include "common.h"
 
-_Context* do_syscall(_Context *c) {
+_Context * do_syscall(_Context * c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
+  a[0] = c->GPR1, a[1] = c->GPR2, a[2] = c->GPR3, a[3] = c->GPR4;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+  case SYS_yield:
+    _yield();
+    c->GPRx = 0;
+    break;
   }
-
   return NULL;
 }
