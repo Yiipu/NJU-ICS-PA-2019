ae433274dc9b90b49f6b4ba8b5cb4c54ffc295fb
202115517-Jin Yapu <jaap20035@outlook.com>
2025-01-16 15:01:28 +0800

    fix typo

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -32,6 +32,6 @@ void __am_vga_init() {
   int i;
   int size = screen_width() * screen_height();
   uint32_t * fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  or (i = 0; i < size; i++) fb[i] = i;
+  for (i = 0; i < size; i++) fb[i] = i;
   draw_sync();
 }
