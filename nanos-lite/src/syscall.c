bb0c647412bb56ac959ab6101428b0d24e94e7a5
tracer-ics2019 <tracer@njuics.org>
2025-01-17 02:36:59 +0800

    >  run 202115517 opal Linux Y7000P-OK 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:36:59 up  5:19,  0 users,  load average: 0.07, 0.18, 0.13 1fda3a319c63662ae1e1b5aa4fbd46a1a6a1d714

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -35,8 +35,6 @@ _Context * do_syscall(_Context * c) {
   uintptr_t a[4];
   a[0] = c->GPR1, a[1] = c->GPR2, a[2] = c->GPR3, a[3] = c->GPR4;
 
-  panic("%d", a[0]);
-
   switch (a[0]) {
   case SYS_yield:
     _yield();
