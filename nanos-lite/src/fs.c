4e84239155f0c18feb54828888887862d387ed5a
tracer-ics2019 <tracer@njuics.org>
2025-01-17 02:48:21 +0800

    >  run 202115517 opal Linux Y7000P-OK 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:48:21 up  5:30,  0 users,  load average: 0.04, 0.11, 0.12 c01cdb0145b9a9e1e17542a9848c52f6fb352a8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,6 +55,7 @@ static inline void fd_check(int fd) {
 }
 
 int fs_open(const char * pathname, int flags, int mode) {
+  printf("%s", pathname);
   for (int i = 0; i < NR_FILES; i++) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       printf("%d\n", i);
