ba0016d9eb365a04fc9ecdc866c297bf986a44ff
tracer-ics2019 <tracer@njuics.org>
2025-01-16 23:13:31 +0800

    >  run 202115517 opal Linux Y7000P-OK 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:13:31 up  1:55,  0 users,  load average: 0.07, 0.04, 0.00 3cb54d8d41c6400e4b5f6e6452816844ab6271c

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -1,14 +1,17 @@
 #include <am.h>
 #include <riscv32.h>
+#include <stdio.h>
 
-static _Context* (*user_handler)(_Event, _Context*) = NULL;
+static _Context * (*user_handler)(_Event, _Context *) = NULL;
 
-_Context* __am_irq_handle(_Context *c) {
-  _Context *next = c;
+_Context * __am_irq_handle(_Context * c) {
+  _Context * next = c;
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {
-      default: ev.event = _EVENT_ERROR; break;
+    default:
+      ev.event = _EVENT_ERROR;
+      break;
     }
 
     next = user_handler(ev, c);
@@ -22,9 +25,11 @@ _Context* __am_irq_handle(_Context *c) {
 
 extern void __am_asm_trap(void);
 
-int _cte_init(_Context*(*handler)(_Event, _Context*)) {
+int _cte_init(_Context * (*handler)(_Event, _Context *)) {
   // initialize exception entry
-  asm volatile("csrw stvec, %0" : : "r"(__am_asm_trap));
+  asm volatile("csrw stvec, %0"
+               :
+               : "r"(__am_asm_trap));
 
   // register event handler
   user_handler = handler;
@@ -32,12 +37,13 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
   return 0;
 }
 
-_Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
+_Context * _kcontext(_Area stack, void (*entry)(void *), void * arg) {
   return NULL;
 }
 
 void _yield() {
   asm volatile("li a7, -1; ecall");
+  printf("123");
 }
 
 int _intr_read() {
