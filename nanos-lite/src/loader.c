0a2eb25686daed141a79ac1a7ecd2277be28c0c6
202115517-Jin Yapu <jaap20035@outlook.com>
2025-01-17 00:10:19 +0800

    format

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,25 +2,25 @@
 #include <elf.h>
 
 #ifdef __ISA_AM_NATIVE__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
-static uintptr_t loader(PCB *pcb, const char *filename) {
+static uintptr_t loader(PCB * pcb, const char * filename) {
   TODO();
   return 0;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB * pcb, const char * filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
 
-void context_kload(PCB *pcb, void *entry) {
+void context_kload(PCB * pcb, void * entry) {
   _Area stack;
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);
@@ -28,7 +28,7 @@ void context_kload(PCB *pcb, void *entry) {
   pcb->cp = _kcontext(stack, entry, NULL);
 }
 
-void context_uload(PCB *pcb, const char *filename) {
+void context_uload(PCB * pcb, const char * filename) {
   uintptr_t entry = loader(pcb, filename);
 
   _Area stack;
