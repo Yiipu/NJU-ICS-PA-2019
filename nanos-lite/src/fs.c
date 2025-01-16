a5769d3151beb83984616adc98c2365d8a47ef87
202115517-Jin Yapu <jaap20035@outlook.com>
2025-01-17 01:49:56 +0800

    文件系统准备工作

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,33 +1,37 @@
 #include "fs.h"
 
-typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
-typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+typedef size_t (*ReadFn)(void * buf, size_t offset, size_t len);
+typedef size_t (*WriteFn)(const void * buf, size_t offset, size_t len);
 
 typedef struct {
-  char *name;
+  char * name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset; // 文件被打开之后的读写指针
   ReadFn read;
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum { FD_STDIN,
+       FD_STDOUT,
+       FD_STDERR,
+       FD_FB };
 
-size_t invalid_read(void *buf, size_t offset, size_t len) {
+size_t invalid_read(void * buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
 }
 
-size_t invalid_write(const void *buf, size_t offset, size_t len) {
+size_t invalid_write(const void * buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
 }
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, invalid_read, invalid_write},
+    {"stdin", 0, 0, invalid_read, invalid_write},
+    {"stdout", 0, 0, invalid_read, invalid_write},
+    {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
